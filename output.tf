fa8d25b4ad865eaac33a7dd6510c97995151589e
jiseooooon <57252187+jiseooooon@users.noreply.github.com>
2022-06-09 10:09:26 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "ec2-private-ip" {
   value = aws_instance.ec2.private_ip
-  description = "The ID of the instance's primary network interface"
+  description = "The ID of the instance's primary network interface; !@#$%^&*=_()<>?"
 }
