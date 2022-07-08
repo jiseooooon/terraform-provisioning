ef67ece85cbcc711d659a396b1c63e706f96fc8b
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-07-08 07:56:31 +0000

    Update from web ide

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "ec2-private-ip" {
   value = aws_instance.ec2.private_ip
-  description = "The ID of the instance's primary network interface; !@#$%^&*=_()<>?"
+  description = "Update from web ide"
 }
